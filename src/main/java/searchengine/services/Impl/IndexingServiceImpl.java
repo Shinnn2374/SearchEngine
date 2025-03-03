0f60f37201f42c41bb11e9a5ee532b98086804e2
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-03 07:40:01 +0300

    ...

--- src/main/java/searchengine/services/Impl/IndexingServiceImpl.java
+++ src/main/java/searchengine/services/Impl/IndexingServiceImpl.java
@@ -10,15 +10,39 @@ import searchengine.utils.LinkExecutor;
 
 import java.time.Instant;
 
+/**
+ * Сервис для работы с индексацией сайтов
+ * Представленные методы служат для начала индексации и ее остановки
+ * @author Shinnn2374
+ * @since 2025-03-03
+ * @see DataBaseRepository
+ */
 
 @Service
 @RequiredArgsConstructor
 public class IndexingServiceImpl implements IndexingService {
 
+    /**
+     * Поле для создания объекта репозитория
+     */
     private final DataBaseRepository repository;
+    /**
+     * Поле для создания объекта - списка сайтов
+     */
     private final SitesList sitesList;
+
+    /**
+     * Поле для создания счетчика, для корректного добавления страниц в таблицу Page
+     */
     private static Integer siteId = 1;
 
+    /**
+     * Метод для начала индексации сайтов из списка SiteList
+     * @see SitesList
+     * @see LinkExecutor
+     * Не получает никаких параметров
+     * @return не возвращает никакое значение
+     */
     @Override
     public Void startIndexing() {
         for (Site site : sitesList.getSites()) {
@@ -32,6 +56,13 @@ public class IndexingServiceImpl implements IndexingService {
         return null;
     }
 
+    /**
+     * Метод для остановки индексации сайтов из списка SiteList
+     * @see SitesList
+     * @see LinkExecutor
+     * Не получает никаких параметров
+     * @return не возвращает никакое значение
+     */
     @Override
     public Void stopIndexing() {
         LinkExecutor.stop();
